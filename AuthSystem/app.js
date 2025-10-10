a2d7f62278cdd62ecb6ef552d354171b9408fd4c
Heyedos <hidosss@hotmail.com>
2025-10-11 00:11:36 +0300

    added AuthSystem miniproject

---
+++ AuthSystem/app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// connection of the mongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("connected"))
+  .catch((error) => console.log("error: ", error));
+
+//Rotues and middlewares
+const authRouter = require("./routes/authRoutes");
+const userRouter = require("./routes/userRoutes");
+const errorHandler = require("./middleware/errorHandler");
+const logger = require("./middleware/logger");
+
+app.use(logger);
+app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
+app.use(errorHandler);
+
+app.listen(process.env.PORT);
--- AuthSystem/routes/userRoutes.js
+++ AuthSystem/routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const verifyToken = require("../middleware/verifyToken");
 const router = express.Router();
+const verifyToken = require("../middleware/verifyToken");
 const User = require("../models/User");
 
 router.get("/profile", verifyToken, async (req, res, next) => {
   try {
-    const user = await User.findById(req.user.id).select("-password"); // güvenlik amaçlı şifresiz gönderiyoruz
-    res.json({ message: "Profile accessed", user });
+    const user = await User.findById(req.user.id).select("-password");
+    res.json({ message: "profile accesed", user });
   } catch (error) {
     next(error);
   }
